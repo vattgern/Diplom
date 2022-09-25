a4324c56543c560af48b587382da8feb860f744b
vattgern <sashawot060@gmail.com>
2022-09-26 00:54:11 +0400

    Just create and tuned laravel&Vue&Vite Bootstrap doesn't work

---
+++ resources/js/components/MainComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <ExampleComponent></ExampleComponent>
+</template>
+
+<script>
+import ExampleComponent from "./ExampleComponent.vue";
+export default {
+    name: "MainComponent",
+    components: {
+        ExampleComponent
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
