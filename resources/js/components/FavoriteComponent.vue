137748493e2ad3c9de9284f7e8b491607a3a3c5b
vattgern <sashawot060@gmail.com>
2022-10-20 15:11:33 +0400

    структура популятного

--- resources/js/components/FavoriteComponent.vue
+++ resources/js/components/FavoriteComponent.vue
@@ -1,5 +1,16 @@
 <template>
+    <section class="favorite">
+        <div class="favorite__video">
 
+        </div>
+        <div class="favorite__header">
+            <h2>Популярное</h2>
+            <a href="#">Больше</a>
+        </div>
+        <div class="favorite__main">
+
+        </div>
+    </section>
 </template>
 
 <script>
