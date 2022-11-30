e35d78e0dd232de135313ffc9fb970249c8d1a3a
vattgern <sashawot060@gmail.com>
2022-12-01 00:28:56 +0400

    Auth not completed

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function signUp(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $user = User::create([
+            'name' =>  $request->input('name'),
+            'login' => $request->input('login'),
+            'password' => Hash::make($request->input('password')),
+            'role' => 'user'
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'code' => 200,
+        ]);
+    }
+    public function signIn(Request $request): \Illuminate\Http\JsonResponse
+    {
+        if(!Auth::attempt($request->all())){
+            return response()->json([
+                'msg' => 'Неверные данные',
+                'code' => 401
+            ]);
+        }
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'code' => 200,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+    public function me(): ?\Illuminate\Contracts\Auth\Authenticatable
+    {
+        return Auth::user();
+    }
+    public function logout(): \Illuminate\Http\JsonResponse
+    {
+        //Удаление токенов
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'msg' => 'Вы вышли'
+        ]);
+    }
+}
